5063a422a5d5db08f45819d6adf435f64d22d3d0
TANABE Hiroyasu <h@cosat.com>
2026-03-08 02:27:20 +0900

    練習問題解答

--- LeanBook/NatCommMonoid/BetterInduction.lean
+++ LeanBook/NatCommMonoid/BetterInduction.lean
@@ -40,5 +40,32 @@ private def MyNat.pred (n : MyNat) : MyNat :=
   | 0 => 0
   | n + 1 => n
 
+private theorem MyNat.pred0: MyNat.pred 0 = 0 := by rfl
+private theorem MyNat.pred1: MyNat.pred 1 = 0 := by rfl
+
 example (n : MyNat) : MyNat.pred (MyNat.pred n + 1) = MyNat.pred n := by
-  sorry
+  induction n
+  case zero =>
+    simp[MyNat.pred0]
+    simp[MyNat.pred1]
+  case succ n' ih =>
+    -- n' : MyNat
+    -- ih : MyNat.pred (MyNat.pred n' + 1) = MyNat.pred n'
+    -- ⊢ MyNat.pred (MyNat.pred (n' + 1) + 1) = MyNat.pred (n' + 1)
+    --
+    -- 左辺 MyNat.pred (MyNat.pred (n' + 1) + 1)
+    -- 内側の MyNat.pred (n' + 1) が 関数定義の n + 1 => n にマッチするため n' になる
+    -- 左辺はその結果として MyNat.pred (n' + 1)
+    -- この時点で 右辺と一致するので rfl でいける
+    rfl
+
+-- 本の解答 最後の rfl を theorem にして rw で適用
+private theorem MyNat.pred_add_one (n : MyNat): MyNat.pred (n + 1) = n := by rfl
+example (n : MyNat) : MyNat.pred (MyNat.pred n + 1) = MyNat.pred n := by
+  rw[MyNat.pred_add_one]
+
+-- Genimi: ケース分割を使う
+example (n : MyNat) : MyNat.pred (MyNat.pred n + 1) = MyNat.pred n := by
+  cases n
+  case zero => rfl -- 0 のケースも計算で解ける
+  case succ n' => rfl -- succ のケースも計算で解ける
