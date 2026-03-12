c4434ce8201b6551f3242324b0ec1db50d074cb4
TANABE Hiroyasu <h@cosat.com>
2026-03-12 23:00:11 +0900

    6.5 順序を保つ 練習問題の解答前まで

---
+++ LeanBook/NatOrder/CompatibleTag.lean
@@ -0,0 +1,5 @@
+import Lean
+
+-- @[compatible] というタグを定義
+/-- compatibleタクティクで使用されるルールを登録する (同一ファイル内では効果を発揮しない) -/
+register_label_attr compatible
--- LeanBook/NatOrder/OrdMonoid.lean
+++ LeanBook/NatOrder/OrdMonoid.lean
@@ -1,4 +1,5 @@
 import LeanBook.NatOrder.NotationSimp
+import LeanBook.NatOrder.CompatibleTag
 
 variable {a b m n : MyNat}
 
@@ -78,3 +79,87 @@ section
     compatible
 
 end
+
+open Lean Elab Tactic in
+
+elab "compatible" : tactic => do
+  -- [compatible] 属性が付与された定理をリストアップする
+  let taggedDecls ← labelled `compatible
+  if taggedDecls.isEmpty then
+    throwError "`[compatible]`が付与された定理はありません"
+  for decl in taggedDecls do
+    let declStx := mkIdent decl
+    try
+      -- [compatible] 属性が付与された定理 thm に対して apply thm <;> assumption を試す
+      evalTactic <| ← `(tactic| apply $declStx <;> assumption)
+      -- 成功したら終了する
+      return ()
+    catch _ =>
+      -- 失敗したら単に次の処理に進む
+      pure ()
+  throwError "ゴールを閉じることができませんでした。"
+
+attribute [compatible] MyNat.add_le_add_left  MyNat.add_le_add_right MyNat.add_le_add
+
+example (h : n ≤ m) (l : MyNat) : l + n ≤ l + m := by compatible
+
+example (h : m ≤ n) (l : MyNat) : m + l ≤ n + l := by compatible
+
+example (h1 : m ≤ n) (h2 : a ≤ b) : m + a ≤ n + b := by compatible
+
+-- ここまで 広義順序。ここから狭義順序 lt になる。(le ではない)
+@[compatible]
+theorem MyNat.add_lt_add_left {m n : MyNat} (h : m < n) (k : MyNat)
+    : k + m < k + n := by
+  notation_simp at *
+  have : k + m + 1 ≤ k + n := calc
+    _ = k + (m + 1) := by ac_rfl
+    _ ≤ k + n := by compatible
+  assumption
+
+@[compatible]
+theorem MyNat.add_lt_add_right {m n : MyNat} (h : m < n) (k : MyNat)
+    : m + k < n + k := calc
+  _ = k + m := by ac_rfl
+  _ < k + n := by compatible
+  _ = n + k := by ac_rfl
+
+section
+
+variable (m n k : MyNat) -- 丸カッコ
+
+theorem MyNat.le_of_add_le_add_left : k + m ≤ k + n → m ≤ n := by
+  intro h
+  rw [MyNat.le_iff_add] at *
+  obtain ⟨d, hd⟩ := h
+  exists d
+  have : m + d + k = n + k := calc
+    _ = k + m + d := by ac_rfl
+    _ = k + n := by rw [hd]
+    _ = n + k := by ac_rfl
+  simp_all
+
+theorem MyNat.le_of_add_le_add_right : m + k ≤ n + k → m ≤ n := by
+  rw [MyNat.add_comm m k, MyNat.add_comm n k]
+  apply MyNat.le_of_add_le_add_left
+
+@[simp] theorem MyNat.add_le_add_iff_left : k + m ≤ k + n ↔ m ≤ n := by
+  constructor
+  · apply MyNat.le_of_add_le_add_left
+  · intro h
+    compatible
+
+@[simp] theorem MyNat.add_le_add_iff_right : m + k ≤ n + k ↔ m ≤ n := by
+  constructor
+  · apply MyNat.le_of_add_le_add_right
+  · intro h
+    compatible
+
+end -- section
+
+-- 練習問題
+variable (m₁ m₂ n₁ n₂ l₁ l₂ : MyNat)
+
+example (h1 : m₁ ≤ m₂) (h2 : n₁ ≤ n₂) (h3 : l₁ ≤ l₂)
+  : l₁ + m₁ + n₁ ≤ l₂ + m₂ + m₂ := calc
+  sorry
